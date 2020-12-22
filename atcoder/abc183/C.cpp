184fe9fa30004da71d8531f75cb7d46b6cc6b2e8
smoking_ace <raghubanshi.shobhit24@gmail.com>
2020-12-23 00:08:00 +0530

    Add solution for problem `C - Travel` Link: https://atcoder.jp/contests/abc183/tasks/abc183_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc183/submissions/18944019

--- atcoder/abc183/C.cpp
+++ atcoder/abc183/C.cpp
@@ -1,36 +1,52 @@
-#include <bits/stdc++.h>
-#define ll long long int
+#include<bits/stdc++.h>
+#define endl       '\n'
+#define pb         push_back
+#define mod        1000000007
+#define int        long long int
+#define hii        cout<<"yes\n";
+#define all(x)     x.begin(),x.end()
+#define deb(x)     cout<<#x<<" : "<<x<<"\n";
+#define FASTIO     ios_base::sync_with_stdio(0);cin.tie(0);cin.tie(0);
 
 using namespace std;
 
-int main() {
-    long long int n,k;
-    cin>>n>>k;
-    int arr[n+1][n+1];
-    for(long long int i = 1; i <= n; ++i){
-        for(ll j = 1; j <= n ; ++j){
-                cin>>arr[i][j];
-        }
-    }
-    vector <ll> cost;
-    for(ll i = 2 ; i <= n ; ++i)
-        cost.push_back(i);
-    ll ans = 0;
-        
-    do{
-        ll cost_price = 0;
-        ll curr = 1;
-        for(ll x : cost){
-            cost_price += arr[curr][x];
-            curr = x;
-        }
-        cost_price += arr[cost.back()][1];
-        
-        if(cost_price == k)
-            ans++;
-        
-    }while(next_permutation(cost.begin(),cost.end()));
-    
-    cout<<ans<<endl;
-    
+void solve() {
+	int n,k;
+	cin>>n>>k;
+	int arr[n+1][n+1];
+	for(int i = 1; i <= n; ++i){
+		for(int j = 1; j <= n; ++j)
+			cin>>arr[i][j];
+	}
+	vector <int> arr_cost;
+	for(int i = 2; i <=n ; ++i){
+		arr_cost.push_back(i);
+	}
+	int ans = 0;
+	do{
+		int curr = 1;
+		int cost = 0;
+		for(int x : arr_cost){
+			cost += arr[curr][x];
+			curr = x;
+		}
+		cost += arr[arr_cost.back()][1];
+		
+		if(cost == k)
+			ans++;
+	
+	}while(next_permutation(all(arr_cost)));
+	
+	cout<<ans<<endl;
+	
+}
+
+int32_t main() {
+	FASTIO;
+	int t = 1;
+	//cin >> t;
+	while (t--) {
+		solve();
+	}
+	return 0;
 }
