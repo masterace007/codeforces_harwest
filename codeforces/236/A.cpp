a875e7e25c8b770312def8fe787573c77f93c6ee
smoking_ace <raghubanshi.shobhit24@gmail.com>
2020-12-31 20:56:00 +0530

    Add solution for problem `A - Boy or Girl` Link: https://codeforces.com/contest/236/problem/A Tags: brute force, implementation, strings, *800 Ref: https://codeforces.com/contest/236/submission/102921890

---
+++ codeforces/236/A.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+#define endl       '\n'
+#define pb         push_back
+#define mod        1000000007
+#define int        long long int
+#define hii        cout<<"yes\n";
+#define all(x)     x.begin(),x.end()
+#define deb(x)     cout<<#x<<" : "<<x<<"\n";
+#define FASTIO     ios_base::sync_with_stdio(0);cin.tie(0);cin.tie(0);
+
+using namespace std;
+
+void solve() {
+	string str;
+	cin >> str;
+	set<char> username;
+	for(int i = 0; i < str.length() ; ++i)
+		username.insert(str[i]);
+	if(username.size() % 2 == 0)
+		cout<<"CHAT WITH HER!"<<endl;
+	else
+		cout<<"IGNORE HIM!"<<endl;
+
+	
+	
+}
+
+int32_t main() {
+	FASTIO;/*
+#ifdef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif*/
+	int t = 1;
+	//cin >> t;
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
