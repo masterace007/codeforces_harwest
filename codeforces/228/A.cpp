6024fb3025994255741c236761195edfcd19e477
smoking_ace <raghubanshi.shobhit24@gmail.com>
2020-12-31 21:55:00 +0530

    Add solution for problem `A - Is your horseshoe on the other hoof?` Link: https://codeforces.com/contest/228/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/228/submission/102925650

---
+++ codeforces/228/A.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+#define endl       '\n'
+#define pb         push_back
+#define mod        1000000007
+#define int        long long int
+#define hii        cout<<"yes\n";
+#define all(x)     x.begin(),x.end()
+#define deb(x)     cout<<#x<<" : "<<x<<"\n";
+#define FASTIO     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+
+using namespace std;
+
+void solve() {
+	set<int> shoes;
+	for(int i = 0; i < 4; ++i){
+	    int a;
+	    cin >> a;
+	    shoes.insert(a);
+	}
+	cout<<4-shoes.size()<<endl;
+}
+
+int32_t main() {
+	FASTIO;/*
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif*/
+	int t = 1;
+	//cin >> t;
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
