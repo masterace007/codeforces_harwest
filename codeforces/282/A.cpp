e521ad5eb115cf5801a91ace054542881893cbbc
smoking_ace <raghubanshi.shobhit24@gmail.com>
2020-12-31 23:54:00 +0530

    Add solution for problem `A - Bit++` Link: https://codeforces.com/contest/282/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/282/submission/102932070

---
+++ codeforces/282/A.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+#define endl       '\n'
+#define pb         push_back
+#define mod        1000000007
+#define int        long long int
+#define hii        cout<<"yes\n";
+#define all(x)     x.begin(),x.end()
+#define deb(x)     cout<<#x<<" : "<<x<<"\n";
+#define FASTIO     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+
+using namespace std;
+
+void solve() {
+	int n;
+	cin >> n;
+	int count = 0;
+	for(int i = 0; i < n; ++i){
+		string str;
+		cin >> str;
+		if(str.find("++") == string::npos)
+			count--;
+		else
+			count++;
+	}
+	cout<<count<<endl;
+}
+
+int32_t main() {
+	FASTIO;/*
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif*/
+	int t = 1;
+	//cin >> t;
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
