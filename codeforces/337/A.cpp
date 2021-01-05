d2bfedf634e3cc62cd8c603568adf6af757aaac7
smoking_ace <raghubanshi.shobhit24@gmail.com>
2021-01-05 16:07:00 +0530

    Add solution for problem `A - Puzzles` Link: https://codeforces.com/contest/337/problem/A Tags: greedy, *900 Ref: https://codeforces.com/contest/337/submission/103369526

---
+++ codeforces/337/A.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+#define endl       '\n'
+#define pb         push_back
+#define mod        1000000007
+#define int        long long int
+#define hii        cout<<"yes\n";
+#define all(x)     x.begin(),x.end()
+#define deb(x)     cout<<#x<<" : "<<x<<"\n";
+#define FASTIO     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+
+using namespace std;
+
+//cout<<hello
+void solve() {
+	int n,m;
+	cin >> n >> m;
+	int arr[m];
+	for(int i = 0; i < m ; ++i)
+		cin >> arr[i];
+
+	sort(arr,arr+m);
+	int min_difference = INT_MAX;
+	for(int i = 0 ; i < m - n + 1 ; ++i){
+		if(min_difference > abs(arr[i] - arr[i+n-1]) ){
+			min_difference = abs(arr[i] - arr[i+n-1]);
+		}
+	}
+	cout<<min_difference<<endl;
+}
+
+int32_t main() {
+	FASTIO;/*
+#ifdef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif*/
+	int t = 1;
+	//cin >> t;
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
