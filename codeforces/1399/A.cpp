9c5f59d8de491da4a140a8f8fd323634877b60e2
smoking_ace <raghubanshi.shobhit24@gmail.com>
2021-01-06 16:34:00 +0530

    Add solution for problem `A - Remove Smallest` Link: https://codeforces.com/contest/1399/problem/A Tags: greedy, sortings, *800 Ref: https://codeforces.com/contest/1399/submission/103532364

---
+++ codeforces/1399/A.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+#define endl       '\n'
+#define pb         push_back
+#define mod        1000000007
+#define int        long long int
+#define hii        cout<<"yes\n";
+#define all(x)     x.begin(),x.end()
+#define deb(x)     cout<<#x<<" : "<<x<<"\n";
+#define FASTIO     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+
+using namespace std;
+
+void solve() {
+	int n;
+	cin >> n;
+	int arr[n];
+	for(int i = 0; i < n ; ++i)
+		cin >> arr[i];
+	sort(arr,arr+n);
+	for(int i = 1; i < n; ++i){
+		if(arr[i] - arr[i-1] > 1){
+			cout<<"NO"<<endl;
+			return;
+		}
+	}
+
+	cout<<"YES"<<endl;
+
+}
+
+int32_t main() {
+	FASTIO;/*
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif*/
+	int t = 1;
+	cin >> t;
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
