75bf104a98746c3d669d01b73b7de84c09ad95fe
smoking_ace <raghubanshi.shobhit24@gmail.com>
2021-01-08 00:44:00 +0530

    Add solution for problem `A - Restoring Three Numbers` Link: https://codeforces.com/contest/1154/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1154/submission/103665048

---
+++ codeforces/1154/A.cpp
@@ -0,0 +1,61 @@
+//
+// Created by smoking_ace on 06-01-2021.
+//
+#include<bits/stdc++.h>
+#define endl       '\n'
+#define pi         3.141592653590
+#define pb         push_back
+#define mod        1000000007
+#define int        long long int
+#define all(x)     x.begin(),x.end()
+#define deb(x)     cout<<#x<<" : "<<x<<"\n";
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x<<" "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+using namespace std;
+using namespace chrono;
+
+void solve() {
+    int ab,ac,bc,abc;
+    cin >> ab >> ac >> bc >> abc;
+    int mx = max(ab,max(ac,max(abc,bc)));
+    int k = 0;
+    int arr[4] = {0};
+    arr[k++] = mx - ab;
+    arr[k++] = mx - ac;
+    arr[k++] = mx - bc;
+    arr[k++] = mx - abc;
+
+    for(int i = 0; i < 4; ++i) {
+        if(arr[i] > 0)
+            cout << arr[i] << " ";
+    }
+    cout<<endl;
+
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    fastio();
+#ifndef ONLINE_JUDGE
+    freopen("Input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+    freopen("Error.txt", "w", stderr);
+#endif
+    auto start1 = high_resolution_clock::now();
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+    auto stop1 = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+    cerr << "Time: " << duration.count() / 1000 << endl;
+#endif
+    return 0;
+}
