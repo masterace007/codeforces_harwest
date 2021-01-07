2f1d77195333935c2a16500261c9c9c9c85602f6
smoking_ace <raghubanshi.shobhit24@gmail.com>
2021-01-07 14:35:00 +0530

    Add solution for problem `A - Fake NP` Link: https://codeforces.com/contest/805/problem/A Tags: greedy, math, *1000 Ref: https://codeforces.com/contest/805/submission/103614996

---
+++ codeforces/805/A.cpp
@@ -0,0 +1,57 @@
+//
+// Created by smoking_ace on 06-01-2021.
+//
+#include<bits/stdc++.h>
+#define endl       '\n'
+#define pi         3.141592653590
+#define pb         push_back
+#define mod        1000000007
+#define int        long long int
+#define all(x)     x.begin(),x.end()
+#define deb(x)     cout<<#x<<" : "<<x<<"\n";
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x<<" "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+using namespace std;
+using namespace chrono;
+
+void solve() {
+    int a,b;
+    cin >> a >> b;
+    if(a<b){
+            cout<<2<<endl;
+    }
+    else if(a == b){
+        if(a % 2 == 0)
+            cout<<2<<endl;
+        else
+            cout<<a<<endl;
+    }else
+        cout<<1<<endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    fastio();
+#ifndef ONLINE_JUDGE
+    freopen("Input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+    freopen("Error.txt", "w", stderr);
+#endif
+    auto start1 = high_resolution_clock::now();
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+    auto stop1 = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+    cerr << "Time: " << duration.count() / 1000 << endl;
+#endif
+    return 0;
+}
