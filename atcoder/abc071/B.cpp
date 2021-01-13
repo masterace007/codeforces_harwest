ed4c85d41da5df87b111e6e739e46362b46c435a
smoking_ace <raghubanshi.shobhit24@gmail.com>
2021-01-13 20:20:00 +0530

    Add solution for problem `B - Not Found` Link: https://atcoder.jp/contests/abc071/tasks/abc071_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc071/submissions/19418220

---
+++ atcoder/abc071/B.cpp
@@ -0,0 +1,49 @@
+#include "bits/stdc++.h"
+#define endl       '\n'
+#define pb         push_back
+#define mod        1000000007
+#define int        long long int
+#define hii        cout<<"yes\n";
+#define all(x)     x.begin(),x.end()
+#define deb(x)     cout<<#x<<" : "<<x<<"\n";
+#define FASTIO     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+
+using namespace std;
+using namespace chrono;
+
+void solve() {
+	string str;
+	cin >> str;
+	set<char> arr;
+	for(auto x : str)
+		arr.insert(x);
+	int num = 97;
+	for(auto x : arr){
+		if((int)x - num == 0)
+			num++;
+		else{
+			cout<<(char)num<<endl;
+			return;
+		}
+	}
+	if(num == 123)
+		cout<<"None"<<endl;
+	else
+		cout<<(char)num<<endl;
+}
+
+int32_t main() {
+	FASTIO; 
+    auto start1 = high_resolution_clock::now();
+	int t = 1;
+	//cin >> t;
+	while (t--) {
+		solve();
+	}
+	auto stop1 = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+    cerr << "Time: " << duration.count() / 1000 << endl;
+#endif
+	return 0;
+}
