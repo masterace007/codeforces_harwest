ac70a24ad31e7f09562f4ce4d8491c67fa356d40
smoking_ace <raghubanshi.shobhit24@gmail.com>
2021-01-16 00:12:00 +0530

    Add solution for problem `A - Lucky Division` Link: https://codeforces.com/contest/122/problem/A Tags: brute force, number theory, *1000 Ref: https://codeforces.com/contest/122/submission/104442136

---
+++ codeforces/122/A.cpp
@@ -0,0 +1,50 @@
+#include "bits/stdc++.h"
+#define endl       '\n'
+#define pb         push_back
+#define mod        1000000007
+#define int        long long int
+#define hii        cout<<"yes\n";
+#define all(x)     x.begin(),x.end()
+#define deb(x)     cout<<#x<<" : "<<x<<"\n";
+#define FASTIO     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+
+using namespace std;
+using namespace chrono;
+
+void solve() {
+    int arr[] = {4,7,47,74,447,477,744,774,747,474,444,44,777,77};
+    int n;
+    cin >> n;
+    for(int i = 0; i < sizeof(arr)/sizeof(arr[0]); ++i){
+        if(n % arr[i] == 0){
+            cout<<"YES"<<endl;
+            return;
+        }
+    }
+
+
+
+    cout<<"NO"<<endl;
+
+}
+
+int32_t main() {
+	FASTIO;
+#ifndef ONLINE_JUDGE
+    freopen("Input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+    freopen("Error.txt", "w", stderr);
+#endif 
+    auto start1 = high_resolution_clock::now();
+	int t = 1;
+	//cin >> t;
+	while (t--) {
+		solve();
+	}
+	auto stop1 = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+    cerr << "Time: " << duration.count() / 1000 << endl;
+#endif
+	return 0;
+}
