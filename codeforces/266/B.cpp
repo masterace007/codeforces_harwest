28cbe7441e5323e4ef0d7425fb9e1c2eab0c7e16
smoking_ace <raghubanshi.shobhit24@gmail.com>
2021-01-17 00:05:00 +0530

    Add solution for problem `B - Queue at the School` Link: https://codeforces.com/contest/266/problem/B Tags: constructive algorithms, graph matchings, implementation, shortest paths, *800 Ref: https://codeforces.com/contest/266/submission/104534228

---
+++ codeforces/266/B.cpp
@@ -0,0 +1,51 @@
+#include "bits/stdc++.h"
+#define endl       '\n'
+#define pb         push_back
+#define mod        1000000007
+#define int        long long int
+#define hii        cout<<"yes\n";
+#define all(x)     x.begin(),x.end()
+#define deb(x)     cout<<#x<<" : "<<x<<"\n";
+#define FASTIO     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+
+using namespace std;
+using namespace chrono;
+
+void solve() {
+	int n,t;
+    cin >> n >> t;
+    string str;
+    cin >> str;
+    for(int i = 0; i < t; ++i){
+        for(int j = 0; j < n-1; ++j){
+            if(str[j] == 'B' && str[j+1] == 'G'){
+                char ch = str[j];
+                str[j] = str[j+1];
+                str[j+1] = ch;
+                j++;
+            }
+        }
+    }
+    cout<<str<<endl;
+}
+
+int32_t main() {
+	FASTIO;
+#ifndef ONLINE_JUDGE
+    freopen("Input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+    freopen("Error.txt", "w", stderr);
+#endif 
+    auto start1 = high_resolution_clock::now();
+	int t = 1;
+	//cin >> t;
+	while (t--) {
+		solve();
+	}
+	auto stop1 = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+    cerr << "Time: " << duration.count() / 1000 << endl;
+#endif
+	return 0;
+}
