b8013728c3d17deac3d0bcee518851a7bd58c442
smoking_ace <raghubanshi.shobhit24@gmail.com>
2021-01-23 02:32:00 +0530

    Add solution for problem `A - Biscuit Generator` Link: https://atcoder.jp/contests/abc125/tasks/abc125_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc125/submissions/19571261

---
+++ atcoder/abc125/A.cpp
@@ -0,0 +1,47 @@
+#include "bits/stdc++.h"
+#define endl       '\n'
+#define pb         push_back
+#define mod        1000000007
+#define int        long long int
+#define hii        cout<<"yes\n";
+#define all(x)     x.begin(),x.end()
+#define deb(x)     cout<<#x<<" : "<<x<<"\n";
+#define FASTIO     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+
+using namespace std;
+using namespace chrono;
+
+void solve() {
+    int a,b,t;
+    cin >> a >> b >> t;
+    //cout<<a<<b<<t<<endl;
+    float t_time = t + 0.5;
+    int count = 0;
+    for(float i = a*1.f ; i <= t_time ; i += a){
+        //cout<<i<<endl;
+        count += b;
+    }
+    cout<<count<<endl;
+}  
+
+
+int32_t main() {
+    FASTIO;
+#ifndef ONLINE_JUDGE
+    freopen("Input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+    freopen("Error.txt", "w", stderr);
+#endif 
+    auto start1 = high_resolution_clock::now();
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+    auto stop1 = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+    cerr << "Time: " << duration.count() / 1000 << endl;
+#endif
+    return 0;
+}
