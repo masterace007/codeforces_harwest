36c25d49fcb74cd59f2fbc955bf6bfcf7e7aa1d1
smoking_ace <raghubanshi.shobhit24@gmail.com>
2021-01-26 01:29:00 +0530

    Add solution for problem `A - Magnets` Link: https://codeforces.com/contest/344/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/344/submission/105417332

---
+++ codeforces/344/A.cpp
@@ -0,0 +1,50 @@
+#include "bits/stdc++.h"
+#define endl       '\n'
+#define pb         push_back
+#define mod        1000000007
+#define int        long long int
+#define hii        cout<<"yes\n";
+#define all(x)     x.begin(),x.end()
+#define deb(x)     cout<<#x<<" : "<<x<<"\n";
+#define FASTIO     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+
+using namespace std;
+using namespace chrono;
+
+void solve() {
+    int n;
+    cin >> n;
+    string str;
+    for(int i = 0; i < n; ++i){
+        string temp;
+        cin >> temp;
+        str.append(temp);
+    }
+    int ans = 0;
+    for(int i = 1; i < str.length()-2; ++i){
+        if(str[i] == str[i+1])
+            ++ans;
+    }
+    cout<<ans+1<<endl;
+}
+
+int32_t main() {
+    FASTIO;
+#ifndef ONLINE_JUDGE
+    freopen("Input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+    freopen("Error.txt", "w", stderr);
+#endif 
+    auto start1 = high_resolution_clock::now();
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+    auto stop1 = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+    cerr << "Time: " << duration.count() / 1000 << endl;
+#endif
+    return 0;
+}
