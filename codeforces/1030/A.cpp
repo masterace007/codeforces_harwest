de57ac38e79bc691e256bf6382d75357eacba19c
smoking_ace <raghubanshi.shobhit24@gmail.com>
2021-01-26 01:19:00 +0530

    Add solution for problem `A - In Search of an Easy Problem` Link: https://codeforces.com/contest/1030/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1030/submission/105416751

---
+++ codeforces/1030/A.cpp
@@ -0,0 +1,49 @@
+#include "bits/stdc++.h"
+#define endl       '\n'
+#define pb         push_back
+#define mod        1000000007
+#define int        long long int
+#define hii        cout<<"yes\n";
+#define all(x)     x.begin(),x.end()
+#define deb(x)     cout<<#x<<" : "<<x<<"\n";
+#define FASTIO     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+
+using namespace std;
+using namespace chrono;
+
+void solve() {
+    int arr[]= {0,0};
+    int n;
+    cin >> n;
+    int a;
+    for(int i = 0; i < n; ++i){
+        cin >> a;
+        arr[a]++;
+    }
+    if(arr[1] >= 1)
+        cout<<"HARD"<<endl;
+    else
+        cout<<"EASY"<<endl;
+
+}
+
+int32_t main() {
+    FASTIO;
+#ifndef ONLINE_JUDGE
+    freopen("Input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+    freopen("Error.txt", "w", stderr);
+#endif 
+    auto start1 = high_resolution_clock::now();
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+    auto stop1 = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+    cerr << "Time: " << duration.count() / 1000 << endl;
+#endif
+    return 0;
+}
