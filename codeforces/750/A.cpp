11304f5ab773f00a77ec96769ca021abe8f2e0eb
smoking_ace <raghubanshi.shobhit24@gmail.com>
2021-01-26 02:27:00 +0530

    Add solution for problem `A - New Year and Hurry` Link: https://codeforces.com/contest/750/problem/A Tags: binary search, brute force, implementation, math, *800 Ref: https://codeforces.com/contest/750/submission/105420275

---
+++ codeforces/750/A.cpp
@@ -0,0 +1,53 @@
+#include "bits/stdc++.h"
+#define endl       '\n'
+#define pb         push_back
+#define mod        1000000007
+#define int        long long int
+#define hii        cout<<"yes\n";
+#define all(x)     x.begin(),x.end()
+#define deb(x)     cout<<#x<<" : "<<x<<"\n";
+#define FASTIO     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+
+using namespace std;
+using namespace chrono;
+
+void solve() {
+    int n,k;
+    cin >> n >> k;
+    int temp = 1;
+    int total_contest = 240 - k;
+    int ans = 0;
+    while(total_contest > 0){
+        if(total_contest - temp*5 >= 0 && temp <= n){
+            total_contest -= (temp*5);
+            ++ans;
+            ++temp;
+
+        }
+        else{
+            break;
+        }
+    }
+    cout<<ans<<endl;
+}
+
+int32_t main() {
+    FASTIO;
+#ifndef ONLINE_JUDGE
+    freopen("Input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+    freopen("Error.txt", "w", stderr);
+#endif 
+    auto start1 = high_resolution_clock::now();
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+    auto stop1 = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+    cerr << "Time: " << duration.count() / 1000 << endl;
+#endif
+    return 0;
+}
