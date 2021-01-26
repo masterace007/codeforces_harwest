e63fe956de89da3cbf9790d6e5e19e70601bd11f
smoking_ace <raghubanshi.shobhit24@gmail.com>
2021-01-26 18:52:00 +0530

    Add solution for problem `B - File Name` Link: https://codeforces.com/contest/978/problem/B Tags: greedy, strings, *800 Ref: https://codeforces.com/contest/978/submission/105482462

---
+++ codeforces/978/B.cpp
@@ -0,0 +1,46 @@
+#include "bits/stdc++.h"
+#define endl       '\n'
+#define pb         push_back
+#define mod        1000000007
+#define int        int64_t
+#define hii        cout<<"yes\n";
+#define all(x)     x.begin(),x.end()
+#define deb(x)     cout<<#x<<" : "<<x<<"\n";
+#define FASTIO     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+
+using namespace std;
+using namespace chrono;
+
+void solve() {
+	int n;
+	cin >> n;
+	string str;
+	cin >> str;
+	int ans = 0;
+	for(int i = 0; i < n-2; ++i){
+		if(str[i] == str[i+2] && str[i+1] ==  str[i+2] && str[i] == 'x')
+			++ans;
+	}
+	cout<<ans<<endl;
+}
+
+signed main() {
+	FASTIO;
+#ifndef ONLINE_JUDGE
+	freopen("Input.txt", "r", stdin);
+	freopen("Output.txt", "w", stdout);
+	freopen("Error.txt", "w", stderr);
+#endif
+	auto start1 = high_resolution_clock::now();
+	int t = 1;
+	//cin >> t;
+	while (t--) {
+		solve();
+	}
+	auto stop1 = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+	cerr << "Time: " << duration.count() / 1000 << endl;
+#endif
+	return 0;
+}
