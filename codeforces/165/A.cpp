f772efd40fa39032c2b53fc1e7000fd2983d5907
smoking_ace <raghubanshi.shobhit24@gmail.com>
2021-01-26 23:51:00 +0530

    Add solution for problem `A - Supercentral Point` Link: https://codeforces.com/contest/165/problem/A Tags: implementation, *1000 Ref: https://codeforces.com/contest/165/submission/105508407

---
+++ codeforces/165/A.cpp
@@ -0,0 +1,84 @@
+//
+// Created by smoking_ace on 06-01-2021.
+//
+#include "bits/stdc++.h"
+#define endl       '\n'
+#define pi         3.141592653590
+#define pb         push_back
+#define mod        1000000007
+#define int        long long int
+#define all(x)     x.begin(),x.end()
+#define deb(x)     cout<<#x<<" : "<<x<<"\n";
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x<<" "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+using namespace std;
+using namespace chrono;
+
+void solve() {
+	int n;
+	cin >> n;
+	vector<pair<int,int> > arr;
+	for(int i = 0; i < n; ++i){
+		int a,b;
+		cin >> a >> b;
+		arr.emplace_back(a,b);
+	}
+	int ans = 0;
+	for(int i = 0; i < n; ++i){
+		bool flag_right = false, flag_left = false,flag_lower = false,flag_upper = false;
+		for (int j = 0; j < n; ++j) {
+			if(arr[j].first == arr[i].first && arr[j].second > arr[i].second ){
+				flag_upper = true;
+				break;
+			}
+		}
+		for (int j = 0; j < n; ++j) {
+			if(arr[j].first == arr[i].first && arr[j].second < arr[i].second ){
+				flag_lower = true;
+				break;
+			}
+		}
+		for (int j = 0; j < n; ++j) {
+			if(arr[j].first > arr[i].first && arr[j].second == arr[i].second ){
+				flag_right = true;
+				break;
+			}
+		}
+		for (int j = 0; j < n; ++j) {
+			if(arr[j].first < arr[i].first && arr[j].second == arr[i].second ){
+				flag_left = true;
+				break;
+			}
+		}
+		if(flag_right && flag_left && flag_lower && flag_upper)
+			++ans;
+	}
+	cout<<ans<<endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    fastio();
+#ifndef ONLINE_JUDGE
+    freopen("Input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+    freopen("Error.txt", "w", stderr);
+#endif
+    auto start1 = high_resolution_clock::now();
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+    auto stop1 = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+    cerr << "Time: " << duration.count() / 1000 << endl;
+#endif
+    return 0;
+}
