8abbc4ea87dfdabd9e41ab88291df425e8d75449
smoking_ace <raghubanshi.shobhit24@gmail.com>
2021-01-27 16:50:00 +0530

    Add solution for problem `A - Design Tutorial: Learn from Math` Link: https://codeforces.com/contest/472/problem/A Tags: math, number theory, *800 Ref: https://codeforces.com/contest/472/submission/105565344

---
+++ codeforces/472/A.cpp
@@ -0,0 +1,48 @@
+#include "bits/stdc++.h"
+#define endl       '\n'
+#define pb         push_back
+#define mod        1000000007
+#define int        int64_t
+#define hii        cout<<"yes\n";
+#define all(x)     x.begin(),x.end()
+#define deb(x)     cout<<#x<<" : "<<x<<"\n";
+#define FASTIO     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+
+using namespace std;
+using namespace chrono;
+
+void solve() {
+	int n;
+	cin >> n;
+	//first even composite no 4 and first odd composite number 9 
+	// is n is odd then it can be formed only by addition of even and odd as odd is know if have to check even;
+	if(n % 2 == 1){
+		int num = n - 9;
+		cout<<9<<" "<<num<<endl;
+	}
+	else{
+		int num = n - 4;
+		cout<<4<<" "<<num<<endl;
+	}
+}
+
+signed main() {
+	FASTIO;
+#ifndef ONLINE_JUDGE
+	freopen("Input.txt", "r", stdin);
+	freopen("Output.txt", "w", stdout);
+	freopen("Error.txt", "w", stderr);
+#endif
+	auto start1 = high_resolution_clock::now();
+	int t = 1;
+	//cin >> t;
+	while (t--) {
+		solve();
+	}
+	auto stop1 = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+	cerr << "Time: " << duration.count() / 1000 << endl;
+#endif
+	return 0;
+}
