636a35a3e7b573b2a97f6fa75c230f70d3b773c8
smoking_ace <raghubanshi.shobhit24@gmail.com>
2021-01-28 18:31:00 +0530

    Add solution for problem `A - If at first you don't succeed...` Link: https://codeforces.com/contest/991/problem/A Tags: implementation, *1000 Ref: https://codeforces.com/contest/991/submission/105668007

---
+++ codeforces/991/A.cpp
@@ -0,0 +1,57 @@
+//
+// Created by smoking_ace on 06-01-2021.
+//
+#include "bits/stdc++.h"
+#define endl       '\n'
+#define pi         3.141592653590
+#define pb         push_back
+#define mod        1000000007
+#define int        long long int
+#define all(x)     x.begin(),x.end()
+#define deb(x)     cout<<#x<<" : "<<x<<"\n";
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x<<" "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+using namespace std;
+using namespace chrono;
+
+void solve() {
+	int a,b,c,n;
+	cin >> a >> b >> c >> n;
+	a -= c;
+	b -= c;
+	if(a < 0 || b < 0)
+		cout<<-1<<endl;
+	else if(a+b+c >= n)
+		cout<<"-1"<<endl;
+	else{
+		n -= (a+b+c);
+		cout<<n<<endl;
+	}
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    fastio();
+#ifndef ONLINE_JUDGE
+    freopen("Input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+    freopen("Error.txt", "w", stderr);
+#endif
+    auto start1 = high_resolution_clock::now();
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+    auto stop1 = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+    cerr << "Time: " << duration.count() / 1000 << endl;
+#endif
+    return 0;
+}
