7561fbd06f3a8413f211395d39b1d5e15b0a0d42
smoking_ace <raghubanshi.shobhit24@gmail.com>
2021-01-29 00:44:00 +0530

    Add solution for problem `A - Theatre Square` Link: https://codeforces.com/contest/1/problem/A Tags: math, *1000 Ref: https://codeforces.com/contest/1/submission/105781355

---
+++ codeforces/1/A.cpp
@@ -0,0 +1,43 @@
+#include "bits/stdc++.h"
+#define endl       '\n'
+#define pb         push_back
+#define mod        1000000007
+#define int        int64_t
+#define hii        cout<<"yes\n";
+#define all(x)     x.begin(),x.end()
+#define deb(x)     cout<<#x<<" : "<<x<<"\n";
+#define FASTIO     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+
+using namespace std;
+using namespace chrono;
+
+void solve() {
+	double n,m,a;
+	cin >> n >> m >> a;
+	
+	int num = ceil(n/a) * ceil(m/a);
+	cout<<num<<endl;
+
+
+}
+
+signed main() {
+	FASTIO;
+#ifndef ONLINE_JUDGE
+	freopen("Input.txt", "r", stdin);
+	freopen("Output.txt", "w", stdout);
+	freopen("Error.txt", "w", stderr);
+#endif
+	auto start1 = high_resolution_clock::now();
+	int t = 1;
+	//cin >> t;
+	while (t--) {
+		solve();
+	}
+	auto stop1 = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+	cerr << "Time: " << duration.count() / 1000 << endl;
+#endif
+	return 0;
+}
