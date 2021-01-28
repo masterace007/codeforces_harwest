063e0df227b3a51f10e986a621fe1a923dae1b4b
smoking_ace <raghubanshi.shobhit24@gmail.com>
2021-01-29 01:31:00 +0530

    Add solution for problem `A - New Year Candles` Link: https://codeforces.com/contest/379/problem/A Tags: implementation, *1000 Ref: https://codeforces.com/contest/379/submission/105784347

---
+++ codeforces/379/A.cpp
@@ -0,0 +1,56 @@
+#include "bits/stdc++.h"
+#define endl       '\n'
+#define pb         push_back
+#define mod        1000000007
+#define int        int64_t
+#define hii        cout<<"yes\n";
+#define all(x)     x.begin(),x.end()
+#define deb(x)     cout<<#x<<" : "<<x<<"\n";
+#define FASTIO     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+
+using namespace std;
+using namespace chrono;
+
+void solve() {
+	int a,b;
+	cin >> a >> b;
+	if(b > a)
+		cout<<a<<endl;
+	else{
+		int cont_minutes = 0;
+		int rem = 0;
+		while(a > 0){
+			cont_minutes += a;
+			rem += a % b;
+			a /= b;
+			if(rem >= b){
+				int temp = rem / b;
+				rem =  rem % b;
+				a += temp;
+			}
+		}
+		//cont_minutes += a;
+		cout<<cont_minutes<<endl;
+	}
+}
+
+signed main() {
+	FASTIO;
+#ifndef ONLINE_JUDGE
+	freopen("Input.txt", "r", stdin);
+	freopen("Output.txt", "w", stdout);
+	freopen("Error.txt", "w", stderr);
+#endif
+	auto start1 = high_resolution_clock::now();
+	int t = 1;
+	//cin >> t;
+	while (t--) {
+		solve();
+	}
+	auto stop1 = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+	cerr << "Time: " << duration.count() / 1000 << endl;
+#endif
+	return 0;
+}
