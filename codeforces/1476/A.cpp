5ca08c4fd70d3c0cf31902bcb7e879ba45ca3b63
smoking_ace <raghubanshi.shobhit24@gmail.com>
2021-01-29 21:03:00 +0530

    Add solution for problem `A - K-divisible Sum` Link: https://codeforces.com/contest/1476/problem/A Tags: binary search, constructive algorithms, greedy, math Ref: https://codeforces.com/contest/1476/submission/105904131

---
+++ codeforces/1476/A.cpp
@@ -0,0 +1,50 @@
+#include "bits/stdc++.h"
+#define endl       '\n'
+#define pb         push_back
+#define mod        1000000007
+#define int        int64_t
+#define hii        cout<<"yes\n";
+#define all(x)     x.begin(),x.end()
+#define deb(x)     cout<<#x<<" : "<<x<<"\n";
+#define FASTIO     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+
+using namespace std;
+using namespace chrono;
+
+void solve() {
+	int n,k;
+	cin >> n >> k;
+	if(n % k == 0){
+		cout<<1<<endl;
+	}
+	else{
+		if(k > n){
+			int num = ceil( k /(double) n);		
+			cout<<num<<endl;
+		}
+		else{
+			cout<<2<<endl;
+		}
+	}
+}
+
+signed main() {
+	FASTIO;
+#ifndef ONLINE_JUDGE
+	freopen("Input.txt", "r", stdin);
+	freopen("Output.txt", "w", stdout);
+	freopen("Error.txt", "w", stderr);
+#endif
+	auto start1 = high_resolution_clock::now();
+	int t = 1;
+	cin >> t;
+	while (t--) {
+		solve();
+	}
+	auto stop1 = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+	cerr << "Time: " << duration.count() / 1000 << endl;
+#endif
+	return 0;
+}
