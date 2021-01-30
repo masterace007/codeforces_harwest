dd56474f3eb4c481b2d17238e3449fad48c49385
smoking_ace <raghubanshi.shobhit24@gmail.com>
2021-01-30 16:15:00 +0530

    Add solution for problem `C - Traveling Salesman around Lake` Link: https://atcoder.jp/contests/abc160/tasks/abc160_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc160/submissions/19770704

---
+++ atcoder/abc160/C.cpp
@@ -0,0 +1,60 @@
+//
+// Created by smoking_ace on 06-01-2021.
+//
+#include "bits/stdc++.h"
+#define endl       '\n'
+#define pi         3.141592653590
+#define pb         push_back
+#define mod        1000000007
+#define int        int64_t
+#define all(x)     x.begin(),x.end()
+#define deb(x)     cout<<#x<<" : "<<x<<"\n";
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x<<" "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false;}
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false;}
+
+using namespace std;
+using namespace chrono;
+
+void solve() {
+	int n,k;
+	cin >> k >> n;
+	int arr[n];
+	for(int i = 0; i < n ; ++i)
+		cin >> arr[i];
+	int min_distance = INT_MAX;
+	min_distance = min(k - (k - arr[n-1]) - arr[0],k - (arr[1] - arr[0]));
+	min_distance = min(min_distance, k - (arr[n-1] - arr[n-2]));
+
+	for(int i = 1; i < n-1; ++i){
+		min_distance = min(min_distance,min(k - (arr[i+1] - arr[i]),k - (arr[i] - arr[i-1])));
+	}
+	cout<<min_distance<<endl;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    fastio();
+#ifndef ONLINE_JUDGE
+    freopen("Input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+    freopen("Error.txt", "w", stderr);
+#endif
+    auto start1 = high_resolution_clock::now();
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+    auto stop1 = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+    cerr << "Time: " << duration.count() / 1000 << endl;
+#endif
+    return 0;
+}
