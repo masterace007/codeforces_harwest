52bfe7aeeb09ab05af7a093c2229da4387139fda
smoking_ace <raghubanshi.shobhit24@gmail.com>
2021-01-31 20:24:00 +0530

    Add solution for problem `A - Sleuth` Link: https://codeforces.com/contest/49/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/49/submission/106113906

---
+++ codeforces/49/A.cpp
@@ -0,0 +1,58 @@
+#include "bits/stdc++.h"
+#define endl       '\n'
+#define pb         push_back
+#define mod        1000000007
+#define int        long long int
+#define hii        cout<<"yes\n";
+#define all(x)     x.begin(),x.end()
+#define deb(x)     cout<<#x<<" : "<<x<<"\n";
+#define FASTIO     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+using namespace std;
+using namespace chrono;
+
+
+void solve() {
+	string str;
+	getline(cin,str);
+	reverse(all(str));
+	for(int i = 0; i < str.length(); ++i){
+		if(str[i] == ' ' || str[i] == '?')
+			continue;
+		else{
+			char c = tolower(str[i]);
+			if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y'){
+				cout<<"YES"<<endl;
+				return;
+			}
+			else{
+				cout<<"NO"<<endl;
+				return;
+			}
+		}
+	}
+
+}
+
+signed main() {
+	FASTIO;
+#ifndef ONLINE_JUDGE
+	freopen("Input.txt", "r", stdin);
+	freopen("Output.txt", "w", stdout);
+	freopen("Error.txt", "w", stderr);
+#endif
+	auto start1 = high_resolution_clock::now();
+	int t = 1;
+	//cin >> t;
+	while (t--) {
+		solve();
+	}
+	auto stop1 = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+	cerr << "Time: " << duration.count() / 1000 << endl;
+#endif
+	return 0;
+}
