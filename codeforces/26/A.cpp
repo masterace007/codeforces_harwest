87ee7b11c013e2084b1c364f965aa91a9265a70a
smoking_ace <raghubanshi.shobhit24@gmail.com>
2021-02-03 16:38:00 +0530

    Add solution for problem `A - Almost Prime` Link: https://codeforces.com/contest/26/problem/A Tags: number theory, *900 Ref: https://codeforces.com/contest/26/submission/106361171

---
+++ codeforces/26/A.cpp
@@ -0,0 +1,71 @@
+//
+// Created by smoking_ace on 06-01-2021.
+//
+#include "bits/stdc++.h"
+#define endl       '\n'
+#define pi         3.141592653590
+#define pb         push_back
+#define mod        1000000007
+#define int        long long int
+#define all(x)     x.begin(),x.end()
+#define deb(x)     cout<<#x<<" : "<<x<<"\n";
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x<<" "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false;}
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false;}
+
+using namespace std;
+using namespace chrono;
+
+bool primecheck(int n){
+	for(int i = 2; i*i <= n; ++i)
+		if(n % i == 0)
+			return false;
+	return true;
+}
+
+void solve() {
+	int n;
+	cin >> n;
+	int count = 0;
+	set<int> arr;
+	for(int i = 6; i < n+1; ++i){
+		int check = 0;
+		for(int j = 2; j <= i/2; ++j){
+			if(i % j == 0){
+				if(primecheck(j)) {
+					check++;
+				}
+			}
+		}
+		if(check == 2)
+			count++;
+	}
+	cout<<count<<endl;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    fastio();
+#ifndef ONLINE_JUDGE
+    freopen("Input.txt", "r", stdin);
+    freopen("Output.txt", "w", stdout);
+    freopen("Error.txt", "w", stderr);
+#endif
+    auto start1 = high_resolution_clock::now();
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+    auto stop1 = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+    cerr << "Time: " << duration.count() / 1000 << endl;
+#endif
+    return 0;
+}
