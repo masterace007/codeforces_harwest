e96a6c960beb596e58ed2e60a250555423eecf25
smoking_ace <raghubanshi.shobhit24@gmail.com>
2021-02-06 03:06:00 +0530

    Add solution for problem `A - Minimum Binary Number` Link: https://codeforces.com/contest/976/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/976/submission/106628239

---
+++ codeforces/976/A.cpp
@@ -0,0 +1,67 @@
+//
+// Created by smoking_ace on 06-01-2021.
+//
+#include "bits/stdc++.h"
+#define endl       '\n'
+#define pi         3.141592653590
+#define pb         push_back
+#define mod        1000000007
+#define int        long long int
+#define all(x)     x.begin(),x.end()
+#define deb(x)     cout<<#x<<" : "<<x<<"\n";
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x<<" "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false;}
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false;}
+
+using namespace std;
+using namespace chrono;
+
+void solve() {
+	int n;
+	cin >> n;
+	string str;
+	cin >> str;
+	int cnt_1 = 0,cnt_0 = 0;
+	for(int i = 0; i < n; ++i)
+		if(str[i] == '1')
+			cnt_1++;
+		else
+			cnt_0++;
+	if(str.length() == 1)
+		cout<<str<<endl;
+	else if(cnt_1 == 1)
+		cout<<str<<endl;
+	else{
+		cout<<1;
+		for(int i = 0; i < cnt_0; ++i)
+			cout<<0;
+		cout<<endl;
+	}
+}
+
+signed main() {
+	ios_base::sync_with_stdio(false);
+	fastio();
+#ifndef ONLINE_JUDGE
+	freopen("Input.txt", "r", stdin);
+	freopen("Output.txt", "w", stdout);
+	freopen("Error.txt", "w", stderr);
+#endif
+	auto start1 = high_resolution_clock::now();
+	int t = 1;
+	//cin >> t;
+	while (t--) {
+		solve();
+	}
+	auto stop1 = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+	cerr << "Time: " << duration.count() / 1000 << endl;
+#endif
+	return 0;
+}
