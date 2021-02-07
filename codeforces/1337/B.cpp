698bdcc97f5147f49ac23cb93452f2eb040e226c
smoking_ace <raghubanshi.shobhit24@gmail.com>
2021-02-08 02:19:00 +0530

    Add solution for problem `B - Kana and Dragon Quest game` Link: https://codeforces.com/contest/1337/problem/B Tags: greedy, implementation, math, *900 Ref: https://codeforces.com/contest/1337/submission/106855908

---
+++ codeforces/1337/B.cpp
@@ -0,0 +1,52 @@
+#include "bits/stdc++.h"
+#define endl       '\n'
+#define pb         push_back
+#define mod        1000000007
+#define int        int64_t
+#define hii        cout<<"yes\n";
+#define all(x)     x.begin(),x.end()
+#define deb(x)     cout<<#x<<" : "<<x<<"\n";
+#define FASTIO     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+using namespace std;
+using namespace chrono;
+
+void solve(){
+	int x,n,m;
+	cin >> x >> n >> m;
+	int min_reach = x;
+	while(n--){
+		x = x/2 + 10;
+		min_reach = min(min_reach,x);
+	}
+	while(m--)
+		min_reach -= 10;
+
+	if(min_reach <= 0)
+		cout<<"YES"<<endl;
+	else
+		cout<<"NO"<<endl;
+}
+
+signed main() {
+	FASTIO;
+#ifndef ONLINE_JUDGE
+	freopen("Input.txt", "r", stdin);
+	freopen("Output.txt", "w", stdout);
+	freopen("Error.txt", "w", stderr);
+#endif
+	auto start1 = high_resolution_clock::now();
+	int t = 1;
+	cin >> t;
+	while (t--) {
+		solve();
+	}
+	auto stop1 = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+	cerr << "Time: " << duration.count() / 1000 << endl;
+#endif
+	return 0;
+}
