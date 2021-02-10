7f9ee0dd98870c10861cf6bf97a691f94f777537
smoking_ace <raghubanshi.shobhit24@gmail.com>
2021-02-10 19:36:00 +0530

    Add solution for problem `A - Pineapple Incident` Link: https://codeforces.com/contest/697/problem/A Tags: implementation, math, *900 Ref: https://codeforces.com/contest/697/submission/107065468

---
+++ codeforces/697/A.cpp
@@ -0,0 +1,51 @@
+#include "bits/stdc++.h"
+#define endl       '\n'
+#define pb         push_back
+#define mod        1000000007
+#define int        int64_t
+#define hii        cout<<"yes\n";
+#define all(x)     x.begin(),x.end()
+#define deb(x)     cout<<#x<<" : "<<x<<"\n";
+#define FASTIO     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+using namespace std;
+using namespace chrono;	
+
+void solve(){
+	int t,s,x;
+	cin >> t >> s >> x;
+	if(x == t)
+		cout<<"YES"<<endl;
+	else if( x < t)
+		cout<<"NO"<<endl;
+	else{
+		x -= t;
+		if(x >= s && (x % s ==  0 || x % s == 1))
+			cout<<"YES"<<endl;
+		else
+			cout<<"NO"<<endl;
+	}
+}
+
+signed main() {
+	FASTIO;
+#ifndef ONLINE_JUDGE
+	freopen("Input.txt", "r", stdin);
+	freopen("Output.txt", "w", stdout);
+	freopen("Error.txt", "w", stderr);
+#endif
+	auto start1 = high_resolution_clock::now();
+	int t = 1;
+	//cin >> t;
+	while (t--) {
+		solve();
+	}
+	auto stop1 = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+	cerr << "Time: " << duration.count() / 1000 << endl;
+#endif
+	return 0;
+}
