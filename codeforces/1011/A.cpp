bb80bc6eb6ff8b1b1b8eb0ce73e66e1021cd4d8d
smoking_ace <raghubanshi.shobhit24@gmail.com>
2021-02-12 00:49:00 +0530

    Add solution for problem `A - Stages` Link: https://codeforces.com/contest/1011/problem/A Tags: greedy, implementation, sortings, *900 Ref: https://codeforces.com/contest/1011/submission/107149265

---
+++ codeforces/1011/A.cpp
@@ -0,0 +1,63 @@
+#include "bits/stdc++.h"
+#define endl       '\n'
+#define pb         push_back
+#define mod        1000000007
+#define int        int64_t
+#define hii        cout<<"yes\n";
+#define all(x)     x.begin(),x.end()
+#define deb(x)     cout<<#x<<" : "<<x<<"\n";
+#define FASTIO     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+using namespace std;
+using namespace chrono;	
+
+void solve(){
+	int n,k;
+	cin >> n >> k;
+	string str;
+	cin >> str;
+	sort(all(str));
+	string ans;
+	ans += str[0];
+	int temp = 1;
+	for(int i = 1; i < n; ++i){
+		if(str[i] - ans[temp-1] >= 2 && temp < k){
+			temp++;
+			ans += str[i];
+		}
+		else if(temp == k)
+			break;
+	}
+	if(ans.length() == k){
+		int weight = 0;
+		for(int i = 0; i < ans.length(); ++i)
+			weight += (ans[i] - 'a' + 1);
+
+		cout<<weight<<endl;
+	}
+	else
+		cout<<-1<<endl;
+}	
+
+signed main() {
+	FASTIO;
+#ifndef ONLINE_JUDGE
+	freopen("Input.txt", "r", stdin);
+	freopen("Output.txt", "w", stdout);
+	freopen("Error.txt", "w", stderr);
+#endif
+	auto start1 = high_resolution_clock::now();
+	int t = 1;
+	//cin >> t;
+	while (t--) {
+		solve();
+	}
+	auto stop1 = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+	cerr << "Time: " << duration.count() / 1000 << endl;
+#endif
+	return 0;
+}
