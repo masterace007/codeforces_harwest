6aa8be2d5500fc5c2a17278bd7664648252ec5b0
smoking_ace <raghubanshi.shobhit24@gmail.com>
2021-02-12 00:33:00 +0530

    Add solution for problem `A - Eating Soup` Link: https://codeforces.com/contest/1163/problem/A Tags: greedy, math, *900 Ref: https://codeforces.com/contest/1163/submission/107148533

---
+++ codeforces/1163/A.cpp
@@ -0,0 +1,48 @@
+#include "bits/stdc++.h"
+#define endl       '\n'
+#define pb         push_back
+#define mod        1000000007
+#define int        int64_t
+#define hii        cout<<"yes\n";
+#define all(x)     x.begin(),x.end()
+#define deb(x)     cout<<#x<<" : "<<x<<"\n";
+#define FASTIO     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+using namespace std;
+using namespace chrono;	
+
+void solve(){
+	int n,m;
+	cin >> n >> m;
+	int num = n/2;
+	if(m > num){
+		cout<<n-m<<endl;
+	}
+	else if(m == 0)
+		cout<<1<<endl;
+	else
+		cout<<m<<endl;
+}	
+
+signed main() {
+	FASTIO;
+#ifndef ONLINE_JUDGE
+	freopen("Input.txt", "r", stdin);
+	freopen("Output.txt", "w", stdout);
+	freopen("Error.txt", "w", stderr);
+#endif
+	auto start1 = high_resolution_clock::now();
+	int t = 1;
+	//cin >> t;
+	while (t--) {
+		solve();
+	}
+	auto stop1 = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+	cerr << "Time: " << duration.count() / 1000 << endl;
+#endif
+	return 0;
+}
