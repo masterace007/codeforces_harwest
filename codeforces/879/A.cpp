68ff621f762db5bbeee65de04fe99c688f5943c8
smoking_ace <raghubanshi.shobhit24@gmail.com>
2021-02-14 21:33:00 +0530

    Add solution for problem `A - Borya's Diagnosis` Link: https://codeforces.com/contest/879/problem/A Tags: implementation, *900 Ref: https://codeforces.com/contest/879/submission/107352700

---
+++ codeforces/879/A.cpp
@@ -0,0 +1,65 @@
+//
+// Created by smoking_ace on 06-01-2021.
+//
+#include "bits/stdc++.h"
+#define endl       '\n'
+#define pi         3.141592653590
+#define pb         push_back
+#define mod        1000000007
+#define int        long long int
+#define all(x)     x.begin(),x.end()
+#define deb(x)     cout<<#x<<" : "<<x<<"\n";
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x<<" "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false;}
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false;}
+
+using namespace std;
+using namespace chrono;
+
+void solve(){
+	int n;
+	cin >> n;
+	vector<pair<int,int>> arr;
+	for(int i = 0; i < n; ++i){
+		int a,b;
+		cin >> a >> b;
+		arr.emplace_back(make_pair(a,b));
+	}
+	int day = arr[0].first;
+	for(int i = 1; i < n; ++i){
+		int temp = arr[i].first;
+		int cnt = 0;
+		while(temp <= day){
+			temp = arr[i].first + (arr[i].second*cnt);
+			cnt++;
+		}
+		day = temp;
+	}
+	cout<<day<<endl;
+}
+signed main() {
+	ios_base::sync_with_stdio(false);
+	fastio();
+#ifndef ONLINE_JUDGE
+	freopen("Input.txt", "r", stdin);
+	freopen("Output.txt", "w", stdout);
+	freopen("Error.txt", "w", stderr);
+#endif
+	auto start1 = high_resolution_clock::now();
+	int t = 1;
+	//cin >> t;
+	while (t--) {
+		solve();
+	}
+	auto stop1 = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+	cerr << "Time: " << duration.count() / 1000 << endl;
+#endif
+	return 0;
+}
