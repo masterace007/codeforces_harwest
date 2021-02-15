b4c44ed4861ef943c8a3a6978ac17656817ce687
smoking_ace <raghubanshi.shobhit24@gmail.com>
2021-02-15 13:29:00 +0530

    Add solution for problem `A - Quasi-palindrome` Link: https://codeforces.com/contest/863/problem/A Tags: brute force, implementation, *900 Ref: https://codeforces.com/contest/863/submission/107387310

---
+++ codeforces/863/A.cpp
@@ -0,0 +1,76 @@
+//
+// Created by smoking_ace on 06-01-2021.
+//
+#include "bits/stdc++.h"
+#define endl       '\n'
+#define pi         3.141592653590
+#define pb         push_back
+#define mod        1000000007
+#define int        long long int
+#define all(x)     x.begin(),x.end()
+#define deb(x)     cout<<#x<<" : "<<x<<"\n";
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x<<" "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false;}
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false;}
+
+using namespace std;
+using namespace chrono;
+
+void solve(){
+	string str;
+	cin >> str;
+	int count = 0;
+	for(char i : str){
+		if(i == '0')
+			count++;
+		else
+			break;
+	}
+	if(count > 0)
+		str = str.substr(count);
+	string temp_reverse;
+	temp_reverse = str;
+	reverse(all(temp_reverse));
+	count = 0;
+	for(char i: temp_reverse)
+		if(i == '0')
+			count++;
+		else
+			break;
+	if(count > 0)
+		temp_reverse = temp_reverse.substr(count);
+	str = temp_reverse;
+	reverse(all(str));
+	if(str == temp_reverse)
+		cout<<"YES"<<endl;
+	else
+		cout<<"NO"<<endl;
+
+}
+
+signed main() {
+	ios_base::sync_with_stdio(false);
+	fastio();
+#ifndef ONLINE_JUDGE
+	freopen("Input.txt", "r", stdin);
+	freopen("Output.txt", "w", stdout);
+	freopen("Error.txt", "w", stderr);
+#endif
+	auto start1 = high_resolution_clock::now();
+	int t = 1;
+	//cin >> t;
+	while (t--) {
+		solve();
+	}
+	auto stop1 = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+	cerr << "Time: " << duration.count() / 1000 << endl;
+#endif
+	return 0;
+}
