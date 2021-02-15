d377070a8b3b5773bdb68d50b8f6f060a7f246ca
smoking_ace <raghubanshi.shobhit24@gmail.com>
2021-02-15 20:19:00 +0530

    Add solution for problem `A - Arena` Link: https://codeforces.com/contest/1487/problem/A Tags: implementation, sortings, *800 Ref: https://codeforces.com/contest/1487/submission/107420698

---
+++ codeforces/1487/A.cpp
@@ -0,0 +1,66 @@
+//
+// Created by smoking_ace on 06-01-2021.
+//
+#include "bits/stdc++.h"
+#define endl       '\n'
+#define pi         3.141592653590
+#define pb         push_back
+#define mod        1000000007
+#define int        long long int
+#define all(x)     x.begin(),x.end()
+#define deb(x)     cout<<#x<<" : "<<x<<"\n";
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x<<" "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false;}
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false;}
+
+using namespace std;
+using namespace chrono;
+
+void solve(){
+	int n;
+	cin >> n;
+	vector<int> arr(n);
+	for(auto &i: arr)
+		cin >> i;
+	if(*max_element(all(arr)) == *min_element(all(arr)))
+		cout<<0<<endl;
+	else{
+		int count = 0;
+		sort(all(arr));
+		int check = arr[0];
+		for(int i = 0; i < n; ++i)
+			if(arr[i] == check)
+				count++;
+			else
+				break;
+		cout<<n-count<<endl;
+	}
+
+}
+
+signed main() {
+	ios_base::sync_with_stdio(false);
+	fastio();
+#ifndef ONLINE_JUDGE
+	freopen("Input.txt", "r", stdin);
+	freopen("Output.txt", "w", stdout);
+	freopen("Error.txt", "w", stderr);
+#endif
+	auto start1 = high_resolution_clock::now();
+	int t = 1;
+	cin >> t;
+	while (t--) {
+		solve();
+	}
+	auto stop1 = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+	cerr << "Time: " << duration.count() / 1000 << endl;
+#endif
+	return 0;
+}
