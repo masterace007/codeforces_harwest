3e0a16ce479b4b0125ce1ab083014d7341be7955
smoking_ace <raghubanshi.shobhit24@gmail.com>
2021-02-17 01:27:00 +0530

    Add solution for problem `B - Disturbed People` Link: https://codeforces.com/contest/1077/problem/B Tags: greedy, *1000 Ref: https://codeforces.com/contest/1077/submission/107637428

---
+++ codeforces/1077/B.cpp
@@ -0,0 +1,60 @@
+//
+// Created by smoking_ace on 06-01-2021.
+//
+#include "bits/stdc++.h"
+#define endl       '\n'
+#define pi         3.141592653590
+#define pb         push_back
+#define mod        1000000007
+#define int        long long int
+#define all(x)     x.begin(),x.end()
+#define deb(x)     cout<<#x<<" : "<<x<<"\n";
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x<<" "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false;}
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false;}
+
+using namespace std;
+using namespace chrono;
+
+void solve(){
+	int n;
+	cin >> n;
+	vector<int> arr(n);
+	for(auto &i: arr)
+		cin >> i;
+	int count = 0;
+	for(int i = 1; i < n-1; ++i){
+		if(arr[i] == 0 && arr[i-1] == 1 && arr[i+1] == 1){
+			count++;
+			arr[i+1] = 0;
+		}
+	}
+	cout<<count<<endl;
+}
+
+signed main() {
+	ios_base::sync_with_stdio(false);
+	fastio();
+#ifndef ONLINE_JUDGE
+	freopen("Input.txt", "r", stdin);
+	freopen("Output.txt", "w", stdout);
+	freopen("Error.txt", "w", stderr);
+#endif
+	auto start1 = high_resolution_clock::now();
+	int t = 1;
+	//cin >> t;
+	while (t--) {
+		solve();
+	}
+	auto stop1 = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+	cerr << "Time: " << duration.count() / 1000 << endl;
+#endif
+	return 0;
+}
