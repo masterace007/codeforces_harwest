ce7ec265a270350e10645f00decef8869bb0db63
smoking_ace <raghubanshi.shobhit24@gmail.com>
2021-02-28 17:13:00 +0530

    Add solution for problem `B1 - TV Subscriptions (Easy Version)` Link: https://codeforces.com/contest/1225/problem/B1 Tags: implementation, *1000 Ref: https://codeforces.com/contest/1225/submission/108663131

---
+++ codeforces/1225/B1.cpp
@@ -0,0 +1,53 @@
+#include "bits/stdc++.h"
+#define endl       '\n'
+#define pb         push_back
+#define mod        1000000007
+#define int        int64_t
+#define hii        cout<<"yes\n";
+#define all(x)     x.begin(),x.end()
+#define deb(x)     cout<<#x<<" : "<<x<<"\n";
+#define FASTIO     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+using namespace std;
+using namespace chrono;
+
+void solve(){
+	int n,k,d;
+	cin >> n >> k >> d;
+	vector<int> arr(n);
+	for(auto &i:arr)
+		cin >> i;
+	int mini_count_element = INT_MAX;
+	for(int i = 0; i <= n-d; ++i){
+		set<int> arr_set;
+		for(int j = i; j < i+d; ++j)
+			arr_set.insert(arr[j]);
+		
+		int temp = arr_set.size();
+		mini_count_element = min(mini_count_element,temp);
+	}
+	cout<<mini_count_element<<endl;
+}
+
+signed main() {
+	FASTIO;
+#ifndef ONLINE_JUDGE
+	freopen("Input.txt", "r", stdin);
+	freopen("Output.txt", "w", stdout);
+	freopen("Error.txt", "w", stderr);
+#endif
+	auto start1 = high_resolution_clock::now();
+	int t = 1;
+	cin >> t;
+	while (t--) {
+		solve();
+	}
+	auto stop1 = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+	cerr << "Time: " << duration.count() / 1000 << endl;
+#endif
+	return 0;
+}
