932fc047d1934bd8ee81c72951ae24dd020458ae
smoking_ace <raghubanshi.shobhit24@gmail.com>
2021-03-06 16:50:00 +0530

    Add solution for problem `A - Lucky Sum of Digits` Link: https://codeforces.com/contest/109/problem/A Tags: brute force, implementation, *1000 Ref: https://codeforces.com/contest/109/submission/109217735

---
+++ codeforces/109/A.cpp
@@ -0,0 +1,57 @@
+#include "bits/stdc++.h"
+#define endl       '\n'
+#define pb         push_back
+#define mod        1000000007
+#define int        int64_t
+#define hii        cout<<"yes\n";
+#define all(x)     x.begin(),x.end()
+#define deb(x)     cout<<#x<<" : "<<x<<"\n";
+#define FASTIO     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+using namespace std;
+using namespace chrono;
+
+void solve(){
+	int n;
+	cin >> n;
+	int num_4 = ceil((double)n/4.f);
+	int num_7 = ceil((double)n/7.f);
+
+	int sum = 0;
+	for(int i = 0; i <= num_4; ++i){
+		for(int j = 0; j <= num_7; ++j){
+			sum = 7 * j + 4 * i;
+			if(sum == n){
+				for(int k = 0; k < i; ++k)
+					cout<<4;
+				for(int k = 0; k < j; ++k)
+					cout<<7;
+				return;
+			}
+		}
+	}
+	cout<<-1<<endl;
+}
+
+signed main() {
+	FASTIO;
+#ifndef ONLINE_JUDGE
+	freopen("Input.txt", "r", stdin);
+	freopen("Output.txt", "w", stdout);
+	freopen("Error.txt", "w", stderr);
+#endif
+	auto start1 = high_resolution_clock::now();
+	int t = 1;
+	//cin >> t;
+	while (t--) {
+		solve();
+	}
+	auto stop1 = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+	cerr << "Time: " << duration.count() / 1000 << endl;
+#endif
+	return 0;
+}
