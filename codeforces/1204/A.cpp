7ff25d44bbbe37336618717f6e71d4b1fadfafa1
smoking_ace <raghubanshi.shobhit24@gmail.com>
2021-03-12 00:57:00 +0530

    Add solution for problem `A - BowWow and the Timetable` Link: https://codeforces.com/contest/1204/problem/A Tags: math, *1000 Ref: https://codeforces.com/contest/1204/submission/109734139

---
+++ codeforces/1204/A.cpp
@@ -0,0 +1,50 @@
+#include "bits/stdc++.h"
+#define endl       '\n'
+#define pb         push_back
+#define mod        1000000007
+#define int        int64_t
+#define hii        cout<<"yes\n";
+#define all(x)     x.begin(),x.end()
+#define deb(x)     cout<<#x<<" : "<<x<<"\n";
+#define FASTIO     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+using namespace std;
+using namespace chrono;
+
+void solve(){
+	string str;
+	cin >> str;
+	int arr[2] = {0};
+	for(char i: str)
+		arr[i-'0']++;
+	if(arr[1] == 0)
+		cout<<0<<endl;
+	else if(arr[1] == 1)
+		cout<<str.length()/2<<endl;
+	else
+		cout<<(str.length()+1)/2<<endl;
+
+}
+
+signed main() {
+	FASTIO;
+#ifndef ONLINE_JUDGE
+	freopen("Input.txt", "r", stdin);
+	freopen("Output.txt", "w", stdout);
+	freopen("Error.txt", "w", stderr);
+#endif
+	auto start1 = high_resolution_clock::now();
+	int t = 1;
+	//cin >> t;
+	while (t--) {
+		solve();
+	}
+	auto stop1 = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifndef ONLINE_JUDGE
+	cerr << "Time: " << duration.count() / 1000 << endl;
+#endif
+	return 0;
+}
